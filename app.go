5a173207be8136eb0e1e6341c33f59fa77786054
Adam Ziolkowski <zett.adam@gmail.com>
2024-08-13 09:47:08 -0500

    Initial code

---
+++ app.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "os"
+)
+
+func main () {
+  hostname, err := os.Hostname()
+  if err != nil {
+    panic(err)
+  }
+
+  http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello world!\nVersion: 1.0.0\n" + hostname + "\n")
+  })
+
+  fs := http.FileServer(http.Dir("static/"))
+  http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+  http.ListenAndServe(":8080", nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zettadam/adamz-api-go
+
+go 1.22.6
